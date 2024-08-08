8146a2b7eeceb0bc6a0df5705bc284dd3e5f6988
WilmerGelves <wilgelgar98@gmail.com>
2024-08-08 16:22:31 -0500

    feat: :white_check_mark: Creación de las funciones globales Limpiar pantalla, pausar pantalla
    Se crearon las funciones  de borrar pantalla y pausar pantalla haciendo uso de la paquetería nativa de python.

---
+++ main.py
@@ -0,0 +1,38 @@
+import funciones.funcionGlobales as globals
+
+def menuPrincipal(op):
+    globals.borrar_pantalla()
+    encabezdo = """
+    **********************
+    * Panadería Pan-Camp *
+    **********************
+    """
+    options = '\t1.Venta.\n\t2.Compra.\n\t3.Gestion al pacientes.\n\t4.Salir'
+    if (op != 3):
+        print(encabezdo)
+        print(options)
+        try:
+            op = int(input('\t=>'))
+        except ValueError:
+            print('Opción inválida...')
+            globals.pausar_pantalla()
+            menuPrincipal(0)
+        else:
+            match (op):
+                case 1:
+                    pass 
+                case 2: 
+                    pass
+                case 3: 
+                    globals.borrar_pantalla()
+                    print('Fue un gusto servirle...Vuelva pronto.')
+                    globals.pausar_pantalla()
+                case _:
+                    globals.borrar_pantalla()
+                    print('Está ingresando una opción inválida...Intente nuevamente.')
+                    globals.pausar_pantalla()
+                    menuPrincipal(0)
+
+
+if __name__ == '__main__':
+    menuPrincipal(0)
